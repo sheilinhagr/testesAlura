77242bf05b916fde723fef88b1f445de6419dd1a
sheilinhagr <shegomesrezende@gmail.com>
2024-06-25 17:55:28 -0300

    alterando para 100

--- js-curso-2-aula_-/app.js
+++ js-curso-2-aula_-/app.js
@@ -4,7 +4,7 @@
 //let paragrafo = document.querySelector('p');
 //paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
